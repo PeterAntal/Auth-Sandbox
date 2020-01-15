64d6b3c0d34eb077fa6b38d96c6f8fdf428b69e3
Peter Antal <172160+PeterAntal@users.noreply.github.com>
2020-01-14 21:16:50 -0800

    Wire up client to call out to server for access token.

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+
+  get '/oauth/callback', to: 'sessions#create'
+  
+  namespace :api do
+    get 'user', to: 'users#show'
+  end
 end
--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,22 +1,13 @@
-class UsersController < ApplicationController
-    def new
-      @user = User.new
-    end
-  
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:success] = "Welcome!"
-        redirect_to root_path
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+lass Api::UsersController < ApplicationController
+  before_action :doorkeeper_authorize!
+
+  def show
+    render json: current_resource_owner.as_json
   end
+
+  private
+
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+end
